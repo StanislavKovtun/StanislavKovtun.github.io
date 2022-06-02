d10afdf0c24396ee2c70e7da5940d381d0f508d1
Stanislav Kovtun <sstanisslaw@gmail.com>
2022-06-02 20:32:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
+import './App.css';
+// import Footer from './Footer.js';
+// 111222
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      
+      <Header/>
+      <Navbar/>
+      <Profile/>
+      
+    </div>
+  );
+}
+// dfgdfgd 
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://logodix.com/logo/1597112.png' />
+    </header>
+    );
+}
+
+export default Header;
