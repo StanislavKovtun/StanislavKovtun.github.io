b5873e2f0f151bcc4b7f165f5a3eebf88950f71c
Stanislav Kovtun <sstanisslaw@gmail.com>
2022-07-26 09:55:08 +0300

    refactoring paginator

--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -3,33 +3,31 @@ import styles from "./Paginator.module.css";
 
 let Paginator = (props) => {
   // debugger;
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let portionCount = Math.ceil(props.totalItemsCount / props.pageSize);
   let pages = [];
-  for (let i = 1; i <= pagesCount; i++) {
+  for (let i = 1; i <= portionCount; i++) {
     pages.push(i);
   }
 
   let curP = props.currentPage;
-  let curPF = curP - 5 < 0 ? 0 : curP - 5;
-  let curPL = curP + 5;
+  let curPF = curP - 10 < 0 ? 0 : curP - 10;
+  let curPL = curP - 10 < 0 ? curP + 9 : curP + 10;
   let slicedPages = pages.slice(curPF, curPL);
-
   return (
-    <div>
-      <div>
-        {slicedPages.map((p) => {
-          return (
-            <span
-              className={props.currentPage === p ? styles.selectedPage : ""}
-              onClick={(e) => {
-                props.onPageChanged(p);
-              }}
-            >
-              {p}
-            </span>
-          );
-        })}
-      </div>
+    <div className={styles.paginator}>
+      {slicedPages.map((p) => {
+        return (
+          <span
+            key={p}
+            className={props.currentPage === p ? styles.selectedPage : ""}
+            onClick={(e) => {
+              props.onPageChanged(p);
+            }}
+          >
+            {p}
+          </span>
+        );
+      })}
     </div>
   );
 };
